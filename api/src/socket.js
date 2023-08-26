ffc24f86db8bf477d0da1b32ce6e0ace3e9b2fa2
SpolavoriBruno <bspol.323@gmail.com>
2023-08-26 12:33:11 -0300

    Implementa autenticação no socket

--- api/src/socket.js
+++ api/src/socket.js
@@ -1,26 +1,56 @@
 const { Server } = require('socket.io')
+const { createAdapter } = require('@socket.io/redis-adapter')
+const { createClient } = require('redis')
 
-const clientLog = console.log.bind(console, "[Socket Client] - ")
+const passport = require('../config/passport')
+const session = require('../config/session')
+const cookieParser = require('cookie-parser')
 
-module.exports = (server) => {
+const {
+    REDIS_URL = "redis://localhost:6379",
+    SECRET = "AVeryStrongSecret"
+} = process.env
+
+const wrap = middleware => (socket, next) => middleware(socket.request, {}, next);
+
+module.exports = async (server) => {
     const io = new Server(server, {
         cors: {
-            origin: "http://localhost:3000"
+            origin: "http://localhost:3000",
+            credentials: true,
         }
     })
 
-    io.on('connection', (socket) => {
-        clientLog(`${socket.id} connected`)
+    const pubClient = createClient({ url: REDIS_URL })
+    const subClient = pubClient.duplicate()
 
-        // socket.emit('connect', `${socket.id} connected`)
+    Promise.all([pubClient.connect(), subClient.connect()])
+        .then(() => io.adapter(createAdapter(pubClient, subClient)))
+
+    io.engine.use(cookieParser(SECRET))
+    io.engine.use(session)
+    io.engine.use(passport.initialize())
+    io.engine.use(passport.session())
+
+    io.use(wrap(require('./middlewares/authMiddleware')))
+
+    io.on('connection', (socket) => {
+        const clientLog = console.log.bind(console, `[Socket Client ${socket.id}] -`)
+        const { user } = socket.request
+
+        clientLog(`User ${user.username} connected`)
+
+        socket.join('global')
+
+        socket.broadcast.emit("online", { timestamp: Date.now(), username: user.username, id: user.id })
 
         socket.on('message', (data) => {
-            clientLog("Message, recived", data)
-            socket.broadcast.emit('message', data)
+            clientLog("Write: ", data)
+            io.to(data.chat).emit("message", data)
         });
 
         socket.on("disconnect", (reason) => {
-            clientLog(`${socket.id} disconnected due to ${reason}`)
+            clientLog(`disconnected due to ${reason}`)
         })
     })
 }
