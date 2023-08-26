ff097ae0f879071dcc16fbaa7d68e1c0550cd85d
SpolavoriBruno <bspol.323@gmail.com>
2023-08-26 12:20:24 -0300

    Aprimora nomes

--- api/src/api.js
+++ api/src/api.js
@@ -1,7 +1,6 @@
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const express = require('express')
-const session = require('express-session')
 const helmet = require('helmet')
 const createError = require('http-errors')
 const logger = require('morgan')
@@ -10,24 +9,36 @@ const passport = require('../config/passport')
 const router = require('./routes')
 
 const app = express()
-const secret = process.env.SECRET || "AVeryStrongSecret"
 
-app.use(logger('dev'))
+const {
+    SECRET = "AVeryStrongSecret",
+    DEBUG_LEVEL = "dev"
+} = process.env
+
+const whitelist = ['http://localhost:3000']
+
+app.use(logger(DEBUG_LEVEL))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cors({
-    origin: "*",
-    credentials: true
-}))
-app.use(session({
-    secret,
-    resave: true,
-    saveUninitialized: true,
-}))
-app.use(cookieParser(secret))
-app.use(passport.local.initialize)
-app.use(passport.local.session)
 app.use(helmet())
+app.use(cors({
+    credentials: true,
+    allowedHeaders: [
+        "Accept",
+        "Content-Type",
+        "Origin",
+        "X-Requested-With",
+    ],
+    origin: (origin, cb) => {
+        whitelist.indexOf(origin) !== -1
+            ? cb(null, true)
+            : cb(new Error('Not allowed by CORS'))
+    },
+}))
+app.use(require('../config/session'))
+app.use(cookieParser(SECRET))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use('/', router)
 
@@ -38,7 +49,7 @@ app.use(function (req, res, next) {
 // error handler
 app.use(function (err, req, res) {
     res.status(err.status || 500)
-    res.json({error: err})
+    res.json({ error: err })
 })
 
 module.exports = app
